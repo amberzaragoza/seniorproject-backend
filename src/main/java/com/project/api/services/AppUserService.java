8410c94b7d38453537bbb7d3efb5a96dd64cc673
jacobfrench <jakefrench1984@gmail.com>
2019-03-11 13:58:56 -0700

    configured Websockets & STOMP

--- src/main/java/com/project/api/services/AppUserService.java
+++ src/main/java/com/project/api/services/AppUserService.java
@@ -1,41 +1,41 @@
-package com.project.api.services;
+// package com.project.api.services;
 
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
 
-import com.project.api.models.AppUser;
+// import com.project.api.models.AppUser;
 
-import org.hibernate.Query;
-import org.hibernate.SessionFactory;
-import org.springframework.stereotype.Service;
-
-
-@Service
-public class AppUserService {
-
-  // public List<AppUser> findOnlineUsers(double latitud, double longitude){
-  //   List<AppUser> users = new ArrayList<>();
-  //   String hqlQuery = "SELECT a FROM AppUser a WHERE (((acos(sin(((:latitude)*pi()/180)) * " +
-  //   "sin((a.latitude*pi()/180))+cos(((:latitude)*pi()/180)) * cos((a.latitude*pi()/180)) * " +
-  //   "cos((((:longitude)- a.longitude)*pi()/180))))*180/pi())*60*1.1515) <=50 AND a.is_online=true";
-
-  //   try{
-  //     Query query = SessionFactory.getCurrentSession().createQuery(hqlQuery);
-  //     query.setDouble("latitude", latitud);
-  //     query.setDouble("longitude", longitude);
-  //     users = (List<AppUser>) query.list();
-
-  //   } catch(Exception e){
-  //     e.printStackTrace();
-  //   }
-  //   return users;
-  // }
+// import org.hibernate.Query;
+// import org.hibernate.SessionFactory;
+// import org.springframework.stereotype.Service;
 
 
+// @Service
+// public class AppUserService {
+
+//   // public List<AppUser> findOnlineUsers(double latitud, double longitude){
+//   //   List<AppUser> users = new ArrayList<>();
+//   //   String hqlQuery = "SELECT a FROM AppUser a WHERE (((acos(sin(((:latitude)*pi()/180)) * " +
+//   //   "sin((a.latitude*pi()/180))+cos(((:latitude)*pi()/180)) * cos((a.latitude*pi()/180)) * " +
+//   //   "cos((((:longitude)- a.longitude)*pi()/180))))*180/pi())*60*1.1515) <=50 AND a.is_online=true";
+
+//   //   try{
+//   //     Query query = SessionFactory.getCurrentSession().createQuery(hqlQuery);
+//   //     query.setDouble("latitude", latitud);
+//   //     query.setDouble("longitude", longitude);
+//   //     users = (List<AppUser>) query.list();
+
+//   //   } catch(Exception e){
+//   //     e.printStackTrace();
+//   //   }
+//   //   return users;
+//   // }
 
 
 
 
 
 
-}
+
+
+// }
